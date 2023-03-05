1ec6c155ec0aceb1f42558fc0026a6cccfb8b40f
SujithaBaskaran <103484001+SujithaBaskaran@users.noreply.github.com>
2023-03-05 14:41:23 +0530

    name update

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Subas Finds';
+    : 'Subas Groups';
 
   return {
     blogTitle,
